b3d6d943b6a8565898450e3a01345c5db090ca05
Abdur Rahman Al Faruqi <abdurrahmanalfaruqi@gmail.com>
2022-09-18 23:06:21 +0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ReturnOfTheCoder.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReturnOfTheCoder.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Person person)
+        {
+            if(isValid(person))
+            {
+                int a = 1;
+                int b = 1;
+                int total = 0;
+                double totalPersonA = 0;
+                double totalPersonB = 0;
+                double Average = 0;
+
+                int nPersonA = person.YearOfDeath1 - person.AgeOfDeath1;
+                int nPersonB = person.YearOfDeath2 - person.AgeOfDeath2;
+                totalPersonA = JumlahDeret(a, b, nPersonA, total);
+                totalPersonB = JumlahDeret(a, b, nPersonB, total);
+                Average = (totalPersonA + totalPersonB) / 2;
+                person.Result = Average;
+                ViewData["Result"] = Average;
+            }
+            else
+            {
+                ViewData["Result"] = -1;
+            }
+            return View();
+        }
+
+        static long JumlahDeret(int a, int b, int n, int Sum)
+        {
+            if (n == 0) 
+                return Sum;
+
+            return JumlahDeret(b, a + b, n - 1, Sum + a);
+        }
+        static bool isValid(Person person)
+        {
+            if (person.AgeOfDeath1 > 0 && person.AgeOfDeath2 > 0 && person.YearOfDeath1 > 0 && person.YearOfDeath2 > 0)
+            {
+                if(((person.YearOfDeath1 - person.AgeOfDeath1)> 0) || ((person.YearOfDeath2 - person.AgeOfDeath2) > 0))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReturnOfTheCoder.Models
+{
+    public class Person
+    {
+        public int AgeOfDeath1 { get; set; }
+        public int YearOfDeath1 { get; set; }
+        public int AgeOfDeath2 { get; set; }
+        public int YearOfDeath2 { get; set; }
+        public double Result { get; set; }
+    }
+}
